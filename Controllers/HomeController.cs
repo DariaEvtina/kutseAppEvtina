f6af0ffb6188bc9b4d2fda5e0d19c2a9f2da0486
opilane <opilane@HP-CZC6297B8P>
2022-03-18 13:41:05 +0200

    gf

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -27,12 +27,13 @@ namespace kutseAppEvtina.Controllers
         public ViewResult Ankeet(Guest guest)
         {
             E_mail(guest);
-            /*if (ModelState.IsValid)
-            {*/
+            if (ModelState.IsValid)
+            {
                 db.Guests.Add(guest);
                 db.SaveChanges();
-                return View("thanks", guest); 
-            /*else { return View(); }*/
+                return View("thanks", guest);
+            }
+            else { return View(); }
 
         }
         public void E_mail(Guest guest)
