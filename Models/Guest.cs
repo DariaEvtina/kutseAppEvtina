f1ad4c37542a047a4866cdce4eccd49ce671b8ab
opilane <opilane@HP-CZC6297B8P>
2022-03-18 14:47:53 +0200

    fxc

--- Models/Guest.cs
+++ Models/Guest.cs
@@ -12,10 +12,10 @@ namespace kutseAppEvtina.Models
         [Required(ErrorMessage = "Sissesta nimi")]
         public string Name { get; set; }
         [Required(ErrorMessage = "Sissesta email")]
-        [RegularExpression(@"\.+\@.+\..+", ErrorMessage = "Vali sisestatud email")]
+        [RegularExpression(@".+\@.+\..+", ErrorMessage = "Vali sisestatud email")]
         public string Email { get; set; }
         [Required(ErrorMessage = "Sissesta telefoni number")]
-        [RegularExpression(@"\+372", ErrorMessage = "Numbril alguses peal olema +372")]
+        [RegularExpression(@"\+372.+", ErrorMessage = "Numbril alguses peal olema +372")]
         public string Phone { get; set; }
         [Required(ErrorMessage = "Sissesta oma valik")]
         public bool? WillAttend { get; set; }
